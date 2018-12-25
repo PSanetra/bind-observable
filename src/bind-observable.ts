4a4aedda6f9729dad89eb435647a183ab16842fd
PSanetra <code@psanetra.de>
2018-12-25 22:17:35 +0100

    fix(BindObservable): Fixes interaction with other decorators which also modify accessors
    Related to https://github.com/PSanetra/bind-observable/issues/5

--- src/bind-observable.ts
+++ src/bind-observable.ts
@@ -1,4 +1,5 @@
 import { ReplaySubject } from 'rxjs'
+import { BindObservableOpts, isBindObservableOpts } from './bind-observable-options'
 
 type SubjectByProp = Map<string, ReplaySubject<any>>
 
@@ -37,35 +38,112 @@ function valueMap(instance: any): ValueByProp {
   return valueMap
 }
 
+function defineObservableProperty(target: Object, observableKey: string): void {
+  Object.defineProperty(target, observableKey, {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return subject(this, observableKey)
+    }
+  })
+}
+
+function redefineSimpleProperty(target: any, propertyKey: string, observableKey: string): void {
+  Object.defineProperty(target, propertyKey, {
+    configurable: true,
+    enumerable: true,
+    set(value) {
+      valueMap(this).set(propertyKey, value)
+      subject(this, observableKey).next(value)
+    },
+    get() {
+      return valueMap(this).get(propertyKey)
+    }
+  })
+}
+
+function redefineAccessorProperty(
+  target: Object,
+  propertyKey: string,
+  observableKey: string,
+  emitRawSetterValue: boolean,
+  descriptor: PropertyDescriptor
+): void {
+  Object.defineProperty(target, propertyKey, {
+    configurable: descriptor.configurable,
+    enumerable: descriptor.enumerable,
+    set(value) {
+      if (!descriptor.set) {
+        throw new Error(`Property ${propertyKey} doesn't have a setter and cannot be written`)
+      }
+
+      descriptor.set.call(this, value)
+
+      const companionProp = subject(this, observableKey)
+
+      if (emitRawSetterValue || !descriptor.get) {
+        companionProp.next(value)
+      } else {
+        companionProp.next(descriptor.get())
+      }
+    },
+    get(): any {
+      if (!descriptor.get) {
+        throw new Error(`Property ${propertyKey} doesn't have a getter and cannot be read`)
+      }
+
+      return descriptor.get.call(this)
+    }
+  })
+}
+
 /**
  * Binds a property to an observable companion property.
  * The observable companion property will emit on all assignments (including initialization),
  * but will not emit undefined if undefined is not explicitly assigned on initialization.
- * @param {string} observableKey
- * optional custom key of the companion property.
- * If not provided, the observableKey is the name of the original property key with a '$' suffix.
+ *
+ * Warning:
+ * Relative declaration position of this decorator to other decorators may be important if another
+ * decorator is transforming the getter return value.
+ *
+ * @param {string | BindObservableOpts} observableKeyOrOpts
+ * provides the companion property key or other additional options.
+ * If the companion property key is not provided, then it will be assumed to be
+ * the name of the original property key + '$' suffix.
  */
-export function BindObservable(observableKey?: string) {
+export function BindObservable(observableKeyOrOpts?: string | BindObservableOpts) {
   return (target: any, propertyKey: string) => {
-    observableKey = observableKey || propertyKey + '$'
+    const opts: BindObservableOpts = isBindObservableOpts(observableKeyOrOpts)
+      ? observableKeyOrOpts
+      : {}
+
+    if (typeof observableKeyOrOpts === 'string') {
+      opts.key = observableKeyOrOpts
+    }
+
+    const observableKey: string = opts.key || propertyKey + '$'
+
+    // The third parameter of this function (descriptor) is passed only if the decorated property
+    // is an accessor, but it won't be passed if another decorator has replaced the descriptor.
+    // See Property Decorators at https://www.typescriptlang.org/docs/handbook/decorators.html
+    // Because of this, we are forced to retrieve the current descriptor with Reflection API
+    const descriptor = Reflect.getOwnPropertyDescriptor(target, propertyKey)
 
     delete target[propertyKey]
     delete target[observableKey]
 
-    Object.defineProperty(target, propertyKey, {
-      set(value) {
-        valueMap(this).set(propertyKey, value)
-        subject(this, observableKey as string).next(value)
-      },
-      get() {
-        return valueMap(this).get(propertyKey)
-      }
-    })
+    defineObservableProperty(target, observableKey)
 
-    Object.defineProperty(target, observableKey, {
-      get() {
-        return subject(this, observableKey as string)
-      }
-    })
+    if (descriptor !== undefined) {
+      redefineAccessorProperty(
+        target,
+        propertyKey,
+        observableKey,
+        !!opts.emitRawSetterValue,
+        descriptor
+      )
+    } else {
+      redefineSimpleProperty(target, propertyKey, observableKey)
+    }
   }
 }
