c74a837db4901a92f739f3d41951049d9b41906d
PSanetra <code@psanetra.de>
2019-06-22 14:02:59 +0200

    chore: Update npm dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -13,8 +13,20 @@ const libraryName = 'bind-observable'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        rxjs: 'rxjs'
+      }
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
